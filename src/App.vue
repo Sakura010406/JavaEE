88e396f0751891046f858233c5ee6f6b8f96a6a7
Sakura010406 <79800321+Sakura010406@users.noreply.github.com>
2021-12-18 20:12:36 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view> </router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  Name: "app",
+  provide() {
+    return {
+      reload: this.reload,
+    };
+  },
+  data() {
+    return {
+      isRouterAlive: true,
+    };
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(function () {
+        this.isRouterAlive = true;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+}
+</style>
+
